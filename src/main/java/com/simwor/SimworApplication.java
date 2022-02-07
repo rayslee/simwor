225c3c615f240e2437b969505026130ca1183bdd
rayslee <raysleeee@gmail.com>
2022-02-07 21:11:02 +0800

    Initial commit

---
+++ src/main/java/com/simwor/SimworApplication.java
@@ -0,0 +1,13 @@
+package com.simwor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimworApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimworApplication.class, args);
+	}
+
+}
