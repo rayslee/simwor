544adff0366bc077a566abce38724639503eceab
raysleeee <raysleeee@gmail.com>
2022-02-09 16:42:28 +0800

    Booker API : /api/booker/query-books-by-title

--- src/main/java/com/simwor/contoller/BookController.java
+++ src/main/java/com/simwor/contoller/BookController.java
@@ -1,11 +1,14 @@
 package com.simwor.contoller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.simwor.bean.Book;
 import com.simwor.mapper.BookMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class BookController {
@@ -16,6 +19,13 @@ public class BookController {
     @GetMapping("/api/booker/query-book-by-isbn")
     public Book queryBookByISBN(@RequestParam("isbn") String isbn) {
         return bookMapper.selectById(isbn);
+    }
+
+    @GetMapping("/api/booker/query-books-by-title")
+    public List<Book> queryBooksByTitle(@RequestParam("title") String title) {
+        QueryWrapper<Book> queryWrapper = new QueryWrapper<>();
+        queryWrapper.select("*").like("title", title);
+        return bookMapper.selectList(queryWrapper);
     }
 
 }
